8e14c948e9cb054a5a93ebcc9eb935e46cd0981f
dikshaaramineni <dikshaa.r.2025@computing.smu.edu.sg>
2026-03-21 23:02:53 +0800

    Added controller, router, and ejs files

--- models/movies-model.js
+++ models/movies-model.js
@@ -1,1 +1,79 @@
+const mongoose = require("mongoose");
 
+const movieSchema = new mongoose.Schema({
+    moviename: {
+        type: String,
+        required: [true, "A movie must have a name"]
+    },
+    movieid: {
+        type: String,
+        required: [true, "A movie must have an ID"],
+        unique: true
+    },
+    description: {
+        type: String,
+        required: [true, "A movie must have a description"]
+    },
+    genre: {
+        type: String,
+        required: [true, "A movie must have a genre"]
+    },
+    ratings: {
+        type: Map,
+        of: Number,
+        default: {}
+    }
+});
+
+const Movie = mongoose.model("Movie", movieSchema, "movies");
+
+exports.getAllMovies = function () {
+    return Movie.find().sort({ moviename: 1 });
+};
+
+exports.getMovieById = function (movieid) {
+    return Movie.findOne({ movieid: movieid });
+};
+
+exports.searchAndFilterMovies = async function (search, genre) {
+    let movies = await Movie.find();
+
+    if (search) {
+        const lowerSearch = search.toLowerCase();
+        movies = movies.filter(movie =>
+            movie.moviename.toLowerCase().includes(lowerSearch)
+        );
+    }
+
+    if (genre && genre !== "all") {
+        movies = movies.filter(movie =>
+            movie.genre === genre
+        );
+    }
+
+    return movies;
+};
+
+exports.getDistinctGenres = function () {
+    return Movie.distinct("genre");
+};
+
+exports.addMovie = function (newMovie) {
+    return Movie.create(newMovie);
+};
+
+exports.updateMovie = async function (movieid, updatedMovie) {
+    const movie = await Movie.findOne({ movieid: movieid });
+
+    if (!movie) return null;
+
+    movie.moviename = updatedMovie.moviename;
+    movie.description = updatedMovie.description;
+    movie.genre = updatedMovie.genre;
+
+    return movie.save();
+};
+
+exports.deleteMovie = function (movieid) {
+    return Movie.deleteOne({ movieid: movieid });
+};
---
+++ routes/movie-routers.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const moviesController = require("./../controllers/movies-controller");
+const authMiddleware = require("./../middleware/authentication");
+
+// Home page (list + search + filter)
+router.get("/home", authMiddleware.isLoggedIn, moviesController.renderHome);
+
+// Add movie
+router.get("/movies/add", authMiddleware.isLoggedIn, moviesController.renderAddMovie);
+router.post("/movies/add", authMiddleware.isLoggedIn, moviesController.addMovie);
+
+// Edit movie
+router.get("/movies/edit", authMiddleware.isLoggedIn, moviesController.renderEditMovie);
+router.post("/movies/edit", authMiddleware.isLoggedIn, moviesController.updateMovie);
+
+// Delete movie
+router.post("/movies/delete", authMiddleware.isLoggedIn, moviesController.deleteMovie);
+
+module.exports = router;
