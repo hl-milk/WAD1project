4a35964347e8c67fc9f2d6e2c284786c9c8f023c
hl-milk <hl.lee.2025@computing.smu.edu.sg>
2026-03-26 13:26:54 +0800

    Clean up

--- routes/watchedFilm-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const watchedController = require('../controllers/watchedFilm-controller');
-const router = express.Router();
-
-router.get('/watched', watchedController.getWatchedList);
-router.post('/watched/delete', watchedController.removeFromWatched);
-
-module.exports = router;
